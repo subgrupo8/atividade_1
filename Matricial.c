d9fcd79da4b66a82c160109c275b08359e572e42
Hugo <hugomaranse@gmail.com>
2025-01-19 19:17:48 -0300

    Adicionado o som do buzzer e a confirmação digitada de tecla pressionada

--- Matricial.c
+++ Matricial.c
@@ -39,14 +39,12 @@ char check_keyboard() {   // Função responsável por ler o teclado
         gpio_put(cols[col], 0); // Coloca as colunas em LOW
 
         for (uint8_t row = 0; row < 4; row++) {
-            if (gpio_get(rows[row]) == 0) { // Detecta se uma tecla foi pressionada
-                sleep_ms(50); // Debounce
-
                 if (gpio_get(rows[row]) == 0) { // Verifica novamente se a tecla ainda está pressionada
+                    sleep_ms(20); //debounce
                     gpio_put(cols[col], 1); // Restaura a coluna para HIGH
                     return key_maps[row * 4 + col]; // index = row * 4 + col
                 }
-            }
+            
         }
         gpio_put(cols[col], 1); // Restaura a coluna para HIGH
     }
@@ -57,26 +55,41 @@ void buzz() { // Configuração do buzzer
     gpio_init(28);
     gpio_set_dir(28, GPIO_OUT);
 }
+void note(uint8_t buzzer, uint16_t notefreq, uint16_t duration_ms){
+    int halfc = 1000000 / (2 * notefreq); // Calcula o tempo de espera para cada meio ciclo de onda
+    int cycles = (notefreq * duration_ms) / 1000; // Número total de ciclos necessários para a duração
 
+    for (i = 0; i < cycles; i++) {
+        gpio_put(28, 1);  // Muda o estado para HIGH iniciando 1º parte do ciclo
+        sleep_us(halfc); // Aguarda metade do tempo de um ciclo (meio período)
+        gpio_put(28, 0);  // Muda o estado para LOW  iniciando 2º parte do ciclo
+        sleep_us(halfc); // Aguarda a outra metade do tempo (meio período)
+    }
+}
 void pressed() {  // Função que faz a relação entre o teclado e os atuadores
     char key;
     key = check_keyboard(); // Verifica qual tecla foi pressionada
-
+    
+    (key != '\0') ? confirm(key) : (void)0 ; 
     // Usando operador ternário para controlar o estado dos LEDs e do buzzer
     (key != '\0' && key == 'A') ? gpio_put(11, 1) : gpio_put(11, 0);
     (key != '\0' && key == 'B') ? gpio_put(12, 1) : gpio_put(12, 0);
     (key != '\0' && key == 'C') ? gpio_put(13, 1) : gpio_put(13, 0);
-    (key != '\0' && key == 'D') ? gpio_put(28, 1) : gpio_put(28, 0);
+    (key != '\0' && key == 'D') ? note(28, 262, 500) : gpio_put(28, 0);
+}
+
+void confirm(char key){
+  printf("Tecla %c pressionada\n", key);
 }
 
 int main(){  // Função principal
+    stdio_init_all();
     ledinitset();
     buzz();
     keyboard();
 
     while(1){ // Laço principal
-        sleep_ms(20);
         pressed();  // Verifica se alguma tecla foi pressionada
-        sleep_ms(100);  // Aguarda um pouco antes de fazer a próxima leitura
+        sleep_ms(150);
     }
 }
